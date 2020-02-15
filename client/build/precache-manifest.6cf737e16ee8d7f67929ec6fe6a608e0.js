b88075021c360cedb74fda2e2e4029cf34dc4204
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-15 01:43:22 -0500

    build

---
+++ client/build/precache-manifest.6cf737e16ee8d7f67929ec6fe6a608e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d839076bf96e1764db3dd051eabb6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9456cd6a178a7120757",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e134e5ff33497143dfeb",
+    "url": "/static/js/2.fa9be03e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fa9be03e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9456cd6a178a7120757",
+    "url": "/static/js/main.353cc6c0.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
