244328490ba2cd785eba350df2040eb20f8fe241
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-15 01:20:32 -0500

    finial init

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,11 +8,17 @@ module.exports = app => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/");
+    }
+  );
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    if (req.user) res.send({ success: "logged out" });
+    res.redirect("/");
   });
 
   app.get("/api/current_user", (req, res) => {
