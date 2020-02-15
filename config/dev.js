4da6e42c276536d00a4041ee7c66fc7fc92ae9b9
Niravpatel129 <niravpatelp129@gmail.com>
2020-02-14 23:04:02 -0500

    init

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev keys
+module.exports = {
+  googleClientID:
+    "690051817221-620ql6s9gcn6m9ktck2c12r1unv7ftp8.apps.googleusercontent.com",
+  googleClientSecret: "Xzc7A06257oJWNTJ_sh1a6QS",
+  mongoURI: "mongodb://admin:dragon1@ds231758.mlab.com:31758/emaily-dev-nirav",
+  cookieKey: "asdakjdnkjsndalkjdnaLKJSDNALKJSDNAklsdjn"
+};
